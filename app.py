f977f299e8dbefa10dfc0fe4dd3e9b159753d88e
Uv-can <indrajitswami20@gmail.com>
2021-06-16 01:48:19 -0400

    New file table_list added and updated app and util

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import sys
+from util import get_tables
 from config import DB_DETAILS
 def main():
-    print('hello world')
-    env = sys.argv[1]
-    db_details = DB_DETAILS[env]
 
-if __name__ == '__main__' :
+    #env = sys.argv[1]
+    #db_details = DB_DETAILS[env]
+    tables = get_tables('table_list.txt')
+    for i in tables['table_name']:
+        print(i)
+if __name__ == '__main__':
     main()
